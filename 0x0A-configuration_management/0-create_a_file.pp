074461d481ae35c51d4393b51cb4cca208829bd8
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-03-24 00:38:10 +0000

    [UPDATE] Change path from path to str declaration on top

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
 # Manifest to create file holberton in /tmp folder
-file {
-  path  =>  '/tmp/holberton',
-  mode  =>  '0744',
-  owner  =>  'www-data',
-  group  =>  'www-data',
-  content  =>  'I love Puppet'
+file { '/tmp/holberton':
+  mode    =>  '0744',
+  owner   =>  'www-data',
+  group   =>  'www-data',
+  content =>  'I love Puppet',
 }
