952981d52fd01e9901b41d1d822683d0dedc0bfa
fvesp18 <1081@holbertonschool.com>
2020-05-05 05:25:26 +0000

    [UPDATE] Add documentation

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
+""" Script will make request to website and format response """"
 if __name__ == "__main__":
     import json
     import requests
     import sys
 
+    # Make request
     emp_req = requests.get("{}users/{}".format(
         "https://jsonplaceholder.typicode.com/", sys.argv[1]))
     todo_req = requests.get("{}users/{}/todos".format(
         "https://jsonplaceholder.typicode.com/", sys.argv[1]))
 
+    # Specify field desired and add counter
     emp_name = emp_req.json().get('name')
     todo = todo_req.json()
     total_tasks = 0
     comp_tasks = 0
 
+    # Iterate through tasks, make total count and completed count
     for tasks in todo:
         total_tasks += 1
         if tasks.get('completed'):
             comp_tasks += 1
 
+    # Format data and print
     print("Employee {} is done with ({}/{}):".format(
         emp_name, comp_tasks, total_tasks))
     for tasks in todo:
